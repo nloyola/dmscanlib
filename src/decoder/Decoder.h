a77319a07e059f03cd80f2850e79f20cf0338b2d
Nelson Loyola <nloyola@gmail.com>
2016-01-14 13:24:10 -0700

    Cleaned up comments in file headers.

--- src/decoder/Decoder.h
+++ src/decoder/Decoder.h
@@ -2,11 +2,6 @@
 #define DECODER_H_
 
 /*
- * Dmscanlib is a software library and standalone application that scans
- * and decodes libdmtx compatible test-tubes. It is currently designed
- * to decode 12x8 pallets that use 2D data-matrix laser etched test-tubes.
- * Copyright (C) 2010 Canadian Biosample Repository
- *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
--- src/test/Tests.cpp
+++ src/test/Tests.cpp
@@ -1,16 +1,4 @@
-/*******************************************************************************
- * Canadian Biosample Repository
- *
- * DmScanLib project
- *
- * Multi-platform application for scanning and decoding datamatrix 2D barcodes.
- *
- * ---------------------------------------------------------------------------
- * Dmscanlib is a software library and standalone application that scans
- * and decodes libdmtx compatible test-tubes. It is currently designed
- * to decode 12x8 pallets that use 2D data-matrix laser etched test-tubes.
- * Copyright (C) 2010 Canadian Biosample Repository
- *
+/**
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -23,8 +11,7 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- ******************************************************************************/
+ */
 
 #define _CRT_SECURE_NO_DEPRECATE
 
